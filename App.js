e345159adba6de0dd6042eb48694c38db962ef83
Hann Hopp <hannah.hopp@gmail.com>
2018-09-15 15:22:06 -0700

    fun with props

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Bananas from './src/components/Bananas';
+
+const Greeting = (props) => {
+
+  return (
+    <Text>Hello { props.name }!</Text>
+  );
+}
 export default class App extends React.Component {
   render() {
+    const names = [ 'Maxine', 'Chloe', 'Rachel' ];
+
     return (
       <View style={styles.container}>
         <Text>Hello world!</Text>
         <Bananas />
+        {
+          names.map((name, i) => (
+            <Greeting key={ i } name={ name } />
+          ))
+        }
       </View>
     );
   }
