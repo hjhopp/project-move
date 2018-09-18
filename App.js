01d2bd1da69d65cc3e6e0b224ac637c5a8f94f60
Hann Hopp <hannah.hopp@gmail.com>
2018-09-17 23:24:53 -0700

    react native tutorial

--- App.js
+++ App.js
@@ -3,23 +3,38 @@ import { StyleSheet, Text, View } from 'react-native';
 import Bananas from './src/components/Bananas';
 
 const Greeting = (props) => {
-
   return (
     <Text>Hello { props.name }!</Text>
   );
 }
 export default class App extends React.Component {
-  render() {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isShowing : true
+    };
+    setInterval(() => {
+      this.setState((previousState) => {
+        return {
+          isShowing : !previousState.isShowing
+        };
+      });
+    }, 1000);
+  }
+
+  render () {
     const names = [ 'Maxine', 'Chloe', 'Rachel' ];
+    console.log(this);
 
     return (
       <View style={styles.container}>
-        <Text>Hello world!</Text>
-        <Bananas />
-        {
-          names.map((name, i) => (
-            <Greeting key={ i } name={ name } />
-          ))
+        { this.state.isShowing ?
+          (
+              <Text>Hello world!</Text>
+          ) :
+          (
+            <Text>Lulz</Text>
+          )
         }
       </View>
     );
